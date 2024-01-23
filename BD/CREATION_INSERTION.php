6adeaa5a1013d5f1a47cf04868f54a60fa214021
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-01-23 12:21:12 +0100

    début bd

--- BD/CREATION_INSERTION.php
+++ BD/CREATION_INSERTION.php
@@ -5,44 +5,93 @@ try{
   // le fichier de BD s'appellera contacts.sqlite3
   $file_db=new PDO('sqlite:contacts.sqlite3');
   $file_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+  // on supprime les tables si elles existent
+  $file_db->exec("DROP TABLE IF EXISTS quiz");
+  $file_db->exec("DROP TABLE IF EXISTS questions");
+  $file_db->exec("DROP TABLE IF EXISTS contacts");
+  $file_db->exec("DROP TABLE IF EXISTS scores");
+
+  $file_db->exec("CREATE TABLE IF NOT EXISTS quiz (
+    id INTEGER PRIMARY KEY,
+    nomQuiz TEXT,
+    meilleurScore INTEGER,
+    tempsMoyen INTEGER,
+    nbMaxQuesitons INTEGER
+  )");
+
   $file_db->exec("CREATE TABLE IF NOT EXISTS contacts ( 
     id INTEGER PRIMARY KEY,
     nom TEXT,
-    prenom TEXT,
-    time INTEGER)");
-
+    prenom TEXT
+  )");
 
   $file_db->exec("CREATE TABLE IF NOT EXISTS scores (
     id INTEGER PRIMARY KEY,
     score INTEGER,
     time INTEGER,
     contact_id INTEGER,
-    FOREIGN KEY(contact_id) REFERENCES contacts(id))");
+    quiz_id INTEGER,
+    FOREIGN KEY(contact_id) REFERENCES contacts(id),
+    FOREIGN KEY(quiz_id) REFERENCES quiz(id)
+  )");
+
+  //table question
+  $file_db->exec("CREATE TABLE IF NOT EXISTS questions (
+    id INTEGER PRIMARY KEY,
+    question TEXT,
+    reponse INTEGER,
+    quiz_id INTEGER,
+    FOREIGN KEY(quiz_id) REFERENCES quiz(id)
+  )");
+
+  $quiz=array(
+    array('nomQuiz' => 'Quiz1',
+      'meilleurScore' => 10,
+      'tempsMoyen' => 20,
+      'nbMaxQuesitons' => 30),
+    array('nomQuiz' => 'Quiz2',
+      'meilleurScore' => 10,
+      'tempsMoyen' => 20,
+      'nbMaxQuesitons' => 30),
+    array('nomQuiz' => 'Quiz3',
+      'meilleurScore' => 10,
+      'tempsMoyen' => 20,
+      'nbMaxQuesitons' => 30)
+  );
 
 
   $contacts=array(
-    array('nom' => 'LAVENANT',
-      'prenom' => 'Jordan',
-      'time' =>  strtotime('17-01-2024')),
-    array('nom' => 'LUCIDOR',
-      'prenom' => 'Léo',
-      'time' =>  strtotime('17-01-2024')),
-    array('nom' => 'DALAIGRE',
-      'prenom' => 'Cristophe',
-      'time' =>  strtotime('17-01-2024'))
+    array('nom' => 'Lucidor',
+      'prenom' => 'Léo'),
+    array('nom' => 'Blandeau',
+      'prenom' => 'Erwan'),
+    array('nom' => 'Lavenant',
+      'prenom' => 'Jordan'),
+    array('nom' => 'Pilet',
+      'prenom' => 'Colin')
     );
 
   $scores=array(
     array('score' => 10,
-      'time' =>  strtotime('17-01-2024'),
-      'contact_id' => 1),
+      'time' => 20,
+      'contact_id' => 1,
+      'quiz_id' => 1),
     array('score' => 20,
-      'time' =>  strtotime('17-01-2024'),
-      'contact_id' => 2),
+      'time' => 30,
+      'contact_id' => 2,
+      'quiz_id' => 1),
     array('score' => 30,
-      'time' =>  strtotime('17-01-2024'),
-      'contact_id' => 3)
-    );
+      'time' => 40,
+      'contact_id' => 3,
+      'quiz_id' => 1),
+  );
+
+  $questions=array(
+    array('question' => 'Quelle est la couleur du cheval blanc d\'Henri IV ?',
+      'reponse' => 1,
+      'quiz_id' => 1),
+  );
   
   $insert="INSERT INTO contacts (nom, prenom, time) VALUES (:nom, :prenom , :time)";
   $stmt=$file_db->prepare($insert);
