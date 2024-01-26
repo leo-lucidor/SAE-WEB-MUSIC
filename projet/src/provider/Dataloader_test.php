b66348b5d5d9e20720576c29f7655535002dbe39
Blandeau Erwan <erwan.blandeau28@gmail.com>
2024-01-26 14:44:17 +0100

    reglage de bug

--- projet/src/provider/Dataloader_test.php
+++ projet/src/provider/Dataloader_test.php
@@ -4,7 +4,6 @@ require 'Dataloader.php';
 require 'Dataloader_function.php';
 
 $dataloader = new Dataloader('database.sqlite3', 'extrait.yml');
-
 $pdo = $dataloader->getPdo();
 deleteAllInBDD($pdo);
 
