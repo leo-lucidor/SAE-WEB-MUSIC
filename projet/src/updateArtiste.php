249eacc468b41b3d8738d67a8a07373f46ca3b37
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-01-30 23:23:49 +0100

    rename un fichier marche pas

--- projet/src/updateArtiste.php
+++ projet/src/updateArtiste.php
@@ -13,6 +13,10 @@ print_r($nomArtiste);
 if ($_REQUEST['nom'] != '' && $_REQUEST['nom'] != trim($nomArtiste)) {
     require 'src/BDD/Function/databaseUpdate.php';
     update_artiste($pdo, $_REQUEST['nom'], $idArtiste);
+
+    require 'src/provider/renameFichier.php';
+    renameFile('./images/ARTISTES', trim($nomArtiste), trim($_REQUEST['nom']));
+
     header('Location: index.php?action=artiste&id='. $idArtiste);
 } else if ($_REQUEST['nom'] == '') {
     header('Location: index.php?action=editerArtiste&idArtiste='. $idArtiste .'&erreur=Veuillez entrer un nom');
