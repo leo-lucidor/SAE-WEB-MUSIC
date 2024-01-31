d135c38529e83cbfc4743377d4481e682e680b7d
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2024-01-31 21:10:56 +0100

    backend des musiques et playlist

--- projet/src/playlist.php
+++ projet/src/playlist.php
@@ -56,7 +56,6 @@ class Playlist {
 
     public function afficher(){
         require 'src/provider/checkFichierDansDossier.php';
-       // require 'src/provider/pdo.php';
         $pdo = getPdo();
         ?>
         <link rel="stylesheet" href="./css/playlist.css">
@@ -64,9 +63,40 @@ class Playlist {
             <a class="btn-retour" href="index.php?action=accueil"><img src="./images/fleche-gauche.png" alt="fleche gauche"></a>
             <?php
                 $nomUser = get_pseudo_with_id($pdo, $this->idUtilisateur);
-                echo '<p class="titre-playlist">'. trim($this->nom) .'<span class="nom-user">Par '. trim($nomUser) .'</span></p>';
+                if (trim($this->nom) == "Titres Likés") {
+                    echo '<p class="titre-playlist">'. trim($this->nom) .'<span class="nom-user">Par Spotiut\'O</span></p>';
+                } else {
+                    echo '<p class="titre-playlist">'. trim($this->nom) .'<span class="nom-user">Par '. trim($nomUser) .'</span></p>';
+                }
             ?>
             <a class="btn-ajouter" href="#"><img src="./images/ajouter.png" alt="ajouter une musique a la playlist"></a>
+        </div>
+        <div class="container-musique">
+            <?php
+            if (count($this->listeMusique) == 0) {
+                echo '<p class="aucune-musique">Aucune musique dans cette playlist</p>';
+            } {
+                foreach ($this->listeMusique as $musique) {
+                    $nomMusique = $musique->getTitle();
+                    $nomArtiste = $musique->getArtist();
+                    $id = $musique->getId();
+                    $chemin = "./MUSIQUES/" .  trim($nomMusique) . ".mp3";
+                    $cheminImage = "./images/MUSIQUES" .  trim($nomMusique) . ".jpg";
+                    $cheminParDefaut = "./images/ALBUMS/default.jpg";
+                    ?>
+                    <div class="musique">
+                        <div class="container-img-musique">
+                            <img class="img-musique" src="<?php echo trim($cheminParDefaut) ?>" alt="image musique">
+                        </div>
+                        <div class="container-info-musique">
+                            <p class="nom-musique"><?php echo $nomMusique ?></p>
+                            <p class="nom-artiste"><?php echo $nomArtiste ?></p>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
         </div>
         <?php
     }
