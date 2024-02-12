a431278d099ff10260e04a19affc1e56cae60515
Coursimault Irvyn <o22206139@info23-14.iut45.univ-orleans.fr>
2024-02-12 17:46:27 +0100

    ajout image titres table

--- src/search_filters/tout.php
+++ src/search_filters/tout.php
@@ -52,7 +52,7 @@
                                     }
                                     echo '<tr>';
                                     echo '<td>';
-                                    echo '<a href="index.php?action=album&id='. trim($album[0]) .'"><img src="' . htmlspecialchars($album[3]) . '" alt="Image de la pochette">' . htmlspecialchars($entry[1]) . '</a>';
+                                    echo '<a href="index.php?action=album&id='. trim($album[0]) .'"><img class="img-titre" src="' . htmlspecialchars($album[3]) . '" alt="Image de la pochette">' . htmlspecialchars($entry[1]) . '</a>';
                                     echo '<td>' . htmlspecialchars($artiste[0]) . '</td>';
                                     echo '<td>' . htmlspecialchars($album[0]) . '</td>';
                                     echo '<td> N/A </td>';
